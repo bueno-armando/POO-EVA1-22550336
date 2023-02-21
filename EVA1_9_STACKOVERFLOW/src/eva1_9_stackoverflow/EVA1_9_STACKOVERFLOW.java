d419c28392a2790a5c7d600e22dd02aab1d7d7e3
bueno-armando <124701314+bueno-armando@users.noreply.github.com>
2023-02-21 13:48:49 -0600

    Add files via upload

---
+++ EVA1_9_STACKOVERFLOW/src/eva1_9_stackoverflow/EVA1_9_STACKOVERFLOW.java
@@ -0,0 +1,16 @@
+package eva1_9_stackoverflow;
+
+/**
+ * @author Armando Bueno
+ */
+public class EVA1_9_STACKOVERFLOW {
+    public static void main(String[] args) {
+        System.out.println("jala");
+        A();
+    }
+    
+    public static void A(){
+        A();
+    }
+    
+}
