24469311e22090527c8bdf8bd898074d9a1456c5
bueno-armando <124701314+bueno-armando@users.noreply.github.com>
2023-02-21 09:29:55 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,15 @@
+package eva1_7_metodos_1;
+
+/**
+ * @author Armando Bueno
+ */
+public class EVA1_7_METODOS_1 {
+    public static void main(String[] args) {
+        myMethod(); //invocar método
+    }
+    
+    public static void myMethod(){
+        System.out.println("You have called me! My name is myMethod");
+    }
+    
+}
