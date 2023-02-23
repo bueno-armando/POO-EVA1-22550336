c69b90d5e58afd2ee14ff26205a616cdbb795905
bueno-armando <124701314+bueno-armando@users.noreply.github.com>
2023-02-23 09:50:24 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,19 @@
+package eva1_15_sobrecarga;
+
+/**
+ * @author Armando Bueno
+ */
+public class EVA1_15_SOBRECARGA {
+
+    public static void main(String[] args) {
+        /** DIFERENTES PRINTLN: SON 10 
+         MÉTDOS, PARA IMPRIMIR DIFERENTES TIPOS DE DATOS
+         LA FIRMA DEL MÉTODO DEBE SER ÚNICA*/
+        System.out.println("HOLA MUNDO!");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args);
+        System.out.println();
+    }
+    
+}
