a2bffa5f30ad6018fd89472816aaaaa3d8e208e6
bueno-armando <124701314+bueno-armando@users.noreply.github.com>
2023-03-03 10:19:15 -0600

    Add files via upload

---
+++ EXAMEN_1RA_EVALUACION_ICE_CREAM_SANDWICH/src/clases_examen/Main.java
@@ -0,0 +1,12 @@
+package clases_examen;
+
+/**
+ *
+ * @author jaime
+ */
+public class Main {
+    public static void main(String[] args){
+        DatosIntegrantes.imprimirDatosEquipo();
+        DatosIntegrantes.imprimirProblemasResueltos();
+    }
+}
